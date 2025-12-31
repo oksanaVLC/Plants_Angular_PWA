ab18a85b0914b717bd6181afd0691b26be726af8
Oksana Golubeva <oksana@uoc.edu>
2025-12-31 16:02:30 +0100

    últimos retoques

--- netlify/functions/plants.js
+++ netlify/functions/plants.js
@@ -1,18 +1,3 @@
-/*export async function handler(event) {
-  const { page = 1, page_size = 100 } = event.queryStringParameters || {};
-
-  const url = `https://trefle.io/api/v1/plants?token=${process.env.TREFLE_TOKEN}&page=${page}&page_size=${page_size}`;
-
-  const response = await fetch(url);
-  const data = await response.json();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data),
-  };
-}
-*/
-
 export async function handler(event) {
   const { page = 1, page_size = 100, id } = event.queryStringParameters || {};
 
